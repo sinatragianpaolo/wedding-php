a79b34432b2bf776614a6b5921e5055e94d8faf2
sinatragianpaolo <gianpaolosinatra@gmail.com>
2024-10-20 02:30:31 +0200

    Refactor getSlugFromUrl function to handle query parameters in the URL

--- features/getSlugFromUrl.php
+++ features/getSlugFromUrl.php
@@ -1,11 +1,17 @@
 <?php
 
-function getSlugFromUrl() : ?string
+function getSlugFromUrl(): ?string
 {
-    $requestUri = $_SERVER['REQUEST_URI']; // Ottieni l'URI richiesto
-    $parts = explode('/', trim($requestUri, '/')); // Dividi l'URI in parti
-    $slug = isset($parts[0]) ? $parts[0] : null; // Prendi la prima parte come slug
+    // Ottieni l'URI richiesto e rimuovi la parte dei parametri di query se presente
+    $requestUri = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+
+    // Dividi l'URI in parti
+    $parts = explode('/', trim($requestUri, '/'));
+
+    // Prendi la prima parte come slug
+    $slug = isset($parts[0]) ? $parts[0] : null;
+
     return $slug;
-    
+
 }
 ?>
