da72487606a68ea03c5802a85efe285fa5ac7bf2
sinatragianpaolo <gianpaolosinatra@gmail.com>
2024-10-29 23:45:53 +0100

    Refactor login.php to check user's wedding ID before verifying password

--- pages/login.php
+++ pages/login.php
@@ -12,16 +12,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'] ?? '';
     $password = $_POST['password'] ?? '';
 
-    $user = getDataFromQuery("SELECT * FROM users WHERE email = :email LIMIT 1", ['email' => $email]) ?? [];
+    $user = getDataFromQuery("SELECT id, weds_id, password FROM users WHERE email = :email LIMIT 1", ['email' => $email]) ?? [];
+    $wedFromSlug = getDataFromQuery("SELECT id FROM weds WHERE slug = :slug LIMIT 1", ['slug' => $slugCouple]) ?? [];
 
-    // Verify password
-    if ($user && password_verify($password, $user['password'])) {
-        // Set session
-        $_SESSION['logged_in'] = true;
-        header("Location: $requestUri?page=backoffice");
-        exit;
+
+
+    if ($user["weds_id"] === $wedFromSlug["id"]) {
+        // Verify password
+        if ($user && password_verify($password, $user['password'])) {
+            // Set session
+            $_SESSION['logged_in'] = true;
+            header("Location: $requestUri?page=backoffice");
+            exit;
+        } else {
+            $error = "Email o password errata!";
+        }
     } else {
-        $error = "Email o password errata!";
+        $error = "Non hai i permessi per accedere a questa pagina!";
     }
 }
 ?>
