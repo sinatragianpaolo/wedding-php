d3446f3b713e2fd990913e9e91c04f085fbfb7b0
sinatragianpaolo <gianpaolosinatra@gmail.com>
2024-11-15 14:15:06 +0100

    Refactor notFound.php to improve error page layout and add dynamic image URLs

--- notFound.php
+++ notFound.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="it">
 
+<?php
+$scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+$host = $_SERVER['HTTP_HOST'];
+$base_url = "$scheme://$host";
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,28 +23,24 @@
             Puoi provare a fare una nuotata oppure a controllare l'URL e ricaricare la pagina. Assicura di aver
             digitato correttamente
             l'indirizzo e di aver indossato le pinne. <br>
-            <?php
-            $scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
-            $host = $_SERVER['HTTP_HOST'];
 
-            $base_url = "$scheme://$host";
-            ?>
             L'URL dovrebbe contenere i nomi degli sposi, eccoti un esempio:
             <strong><?= $base_url ?>/alice-e-bob</strong><br>
             Se il problema persiste, contatta il supporto tecnico, scrivendo alla mail: <br>
             g-s@example.com
         </p>
         <div class="image-container">
-            <img src="assets/images/404_funny.png" alt="Pagina non trovata" class="funny-image">
-            <div class="error-number">404</div> <!-- Aggiunto il div per il numero 404 -->
+            <img src="<?= $base_url ?>/assets/images/404_funny.png" alt="Pagina non trovata" class="funny-image img-1">
+            <img src="<?= $base_url ?>/assets/images/wedding_404.png" alt="Pagina non trovata" class="funny-image img-2">
         </div>
-        <div class="buttons">
-            <a href="javascript:history.back()" class="btn-back">🔙 Torna indietro</a>
-        </div>
+    </div>
+    <div class="buttons">
+        <a href="javascript:history.back()" class="btn-back">🔙 Torna indietro</a>
+    </div>
 
-        <div class="emoji">
-            🌟🌈💥🎈🎉
-        </div>
+    <div class="emoji">
+        🌟🌈💥🎈🎉
+    </div>
     </div>
 </body>
 
